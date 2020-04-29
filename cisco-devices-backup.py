8c4e1f87df82e8688347116c1c8df311d73623d4
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-04-29 11:18:05 +0900

    initialized new repo for devnet marathon April 2020

---
+++ cisco-devices-backup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# """Module docstring."""
+
+#Imports
+from netmiko import ConnectHandler
+import csv
+import logging
+import datetime
+import multiprocessing as mp
+import difflib
+import filecmp
+import sys
+import os
